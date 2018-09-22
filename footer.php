550aee0bb63abfb839ac3ee01087a4a50de92c8c
RiskaSintiaDewi <42725266+RiskaSintiaDewi@users.noreply.github.com>
2018-09-21 20:54:45 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
 <br><br>
 <table bgcolor="pink" width="100%" height="5%">
 	<tr>
-	<td></td>
+	<td><a href="https://www.instagram.com/riskasintiad/">Instagram
+		<a href="https://www.facebook.com/">Facebook</td>
 </tr>
 </table>
